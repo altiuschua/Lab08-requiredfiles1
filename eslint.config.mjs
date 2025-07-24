dd4b0fa10d4b44fe1f266f26235db792d6943f2f
altiuschua <altiuschua99@gmail.com>
2025-07-24 16:16:13 +0800

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,21 @@
-import react from 'eslint-plugin-react';
-import globals from 'globals';
-
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginSecurity from "eslint-plugin-security";
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-    plugins: {
-      react,
-    },
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        ...globals.browser,
-      },
-    },
-    rules: {
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-    },
-    // ... others are omitted for brevity
-  },
+{files: ["**/*.{js,mjs,cjs,jsx}"]},
+{languageOptions: { globals: globals.browser }},
+pluginJs.configs.recommended,
+pluginReact.configs.flat.recommended,
+{
+plugins: {
+security: pluginSecurity
+},
+rules: {
+...pluginJs.configs.recommended.rules,
+...pluginReact.configs.flat.recommended.rules,
+"security/detect-eval-with-expression": "error",
+}
+}
 ];
